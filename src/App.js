090204bf18faf176eb412c0ea6c017a199865827
Bill Deng <billdevcode@gmail.com>
2017-09-10 09:57:51 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ViewLayout from './layouts/ViewLayout'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ViewLayout />
+      </div>
+    );
+  }
+}
+
+export default App;
