c3742af060c167bd68088970b5091c146542e8e7
Bill Deng <billdevcode@gmail.com>
2017-09-10 20:32:18 -0700

    Add Search feature

---
+++ src/Search/Search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Search = ({ submit }) => {
+    return (
+        <div className='Search'>
+            <form onSubmit={submit}>
+                <input className='Search-searchbox' type='text' name='username' placeholder='Search user' required/>
+                <input className='Search-submitButton' type='submit' />
+            </form>
+        </div>
+    )
+}
+
+export default Search
