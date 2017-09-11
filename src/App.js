23ad6ba15db911ef89db275289c32534b00b2a02
Bill Deng <billdevcode@gmail.com>
2017-09-10 20:32:03 -0700

    Add UserLayout and link to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import ViewLayout from './layouts/ViewLayout'
+import UserLayout from './layouts/UserLayout'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ViewLayout />
+        <UserLayout />
       </div>
     );
   }
